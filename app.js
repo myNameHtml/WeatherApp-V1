cb9ef6827f7aed2ae1226a824a7d6d2159659848
Kyaw Htet Htun <kyawhtethtun65@.com>
2021-08-29 14:31:42 +0200

    Text in html edited and standard background added

--- app.js
+++ app.js
@@ -16,23 +16,21 @@ window.addEventListener("load", () => {
     let long;
     let lat;
     
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-        const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`;
-        fetch(api)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            const { name } = data;
-            const { feels_like, temp, temp_max, temp_min } = data.main;
-            const { id, icon, main } = data.weather[0];
-            ShowData(data)
-          });
-      });
-    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`;
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { name } = data;
+          const { feels_like, temp, temp_max, temp_min } = data.main;
+          const { id, icon, main } = data.weather[0];
+          ShowData(data)
+        });
+    });
 });
 
 searchBtn.addEventListener("click", function(){
